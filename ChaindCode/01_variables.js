868a105ae94aa75273de2381fbc912d0ac183594
shivajais25 <shivajais25@gmail.com>
2026-03-01 18:44:16 +0530

    variables and datatypes

---
+++ ChaindCode/01_variables.js
@@ -0,0 +1,5 @@
+const accountId = 12422123  //can't be changed once created
+let accountName = "John Doe"    //used to assign for fluctuating values
+var accountBalance = 1000.50 // not use var in modern js beacause of block and functional scope
+
+console.table([accountId, accountName, accountBalance]) // used to print variable in tabular format 
---
+++ ChaindCode/02_datatypes.js
@@ -0,0 +1,12 @@
+"use strict"; // treat all JS code as newer version 
+
+//alert( 3 + 3) // we are using alert in node js, not browser
+let Name = "Shiva Jaiswal"
+let age = 20
+let state;
+console.table([Name, age, state])
+
+console.log(typeof Name)        // type of Name is => string
+console.log(typeof age)         // type of age is => number
+console.log(typeof undefined)   // type of undefined is => undefined
+console.log(typeof null)        // type of null is object => standalone value
