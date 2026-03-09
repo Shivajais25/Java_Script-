20bb512b595664aeae8dc858b693a5ea074ca9b3
shivajais25 <shivajais25@gmail.com>
2026-03-09 21:38:47 +0530

    stack and heap included

--- ChaindCode/04_datatypes_summary.js
+++ ChaindCode/04_datatypes_summary.js
@@ -29,3 +29,28 @@ const myFunction = function(){
 console.log(typeof languages)
 console.log(typeof myObj)
 console.log(typeof myFunction)
+
+
+/* ++++++++++++++++++++++++++++++++++++++++++++++
+    Stack memory(Primitive) ==> store copy (does not change original value)
+    , Heap memory(Non-primitive or reference)   ==> give reference or original value 
+
+    Below is the example of Stack */
+    let my_lang = "Javascript"
+    your_lang = my_lang
+    console.log(your_lang)      // here we get the copy of my_lang which is primitive type so if we do any
+                                // change in your_lang so my_lang value will always be "Javascript"
+    your_lang = "typescript"
+    console.log(your_lang)
+
+    // Below is the example of Heap 
+
+    let userOne = {
+        email : "230354@kit.ac.in",
+        course : "BCA"
+    }
+    let userTwo = userOne      // here we get the original values because it is stored in heap memory
+    console.log(userTwo)    // here we get the original value 
+    userTwo.email = "shivajais25@gmail.com"     // here original value is changed so the userOne and userTwo mail now will be same
+    console.log(userOne)
+    console.log(userTwo)
