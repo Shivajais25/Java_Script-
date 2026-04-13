003f904d6b2c743163a9991946b6a799f4b5ab16
shivajais25 <shivajais25@gmail.com>
2026-04-13 21:42:10 +0530

    while and do-while loop

---
+++ ChaindCode/05_Iterations/02_loop.js
@@ -0,0 +1,28 @@
+// while and do-while loop
+
+let index = 0
+
+while (index <= 10) {
+    console.log(index)
+    index = index + 2
+}
+
+let myarray = ['cotlin', 'java', 'python', 'rust']
+
+let arr_idx = 0
+
+while (arr_idx < myarray.length) {
+    console.log(myarray[arr_idx])
+
+    arr_idx++
+}
+
+
+// do-while loop
+
+let score = 12
+do {
+
+    console.log(score)
+
+} while (score <= 10);
