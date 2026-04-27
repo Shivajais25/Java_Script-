282d37e203ebd955f2ece055e5a963bccfbba81a
shivajais25 <shivajais25@gmail.com>
2026-04-27 20:28:00 +0530

    for-in loop for objects

---
+++ ChaindCode/05_Iterations/04_loop.js
@@ -0,0 +1,13 @@
+// for-in loop is used for object iteration mainly 
+
+const myobject = {
+    js: "javascript",
+    cpp: "c++",
+    rb: "ruby",
+    py: "python"
+} 
+
+for (const key in myobject) {
+    
+    console.log(key)
+}
