9ba3de9bebbefb093f140ab25a47757218f53b8f
shivajais25 <shivajais25@gmail.com>
2026-05-26 12:50:45 +0530

    promises uing parameter in resolve

--- ChaindCode/09_advance_one/promises.js
+++ ChaindCode/09_advance_one/promises.js
@@ -11,3 +11,18 @@ const promiseOne = new Promise(function(resolve, reject){
 promiseOne.then(function(){
     console.log("Async task resolved!")
 })
+
+// promise with parameters
+
+const promiseTwo = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        resolve({username : "Shiva Jaiswal", email : "shivajais25@gmail.com"})  //passing parameters in resolve 
+    }, 3000)
+})
+
+promiseTwo.then(function(user){
+    console.log(user)
+})
+
+
+// using of reject 
