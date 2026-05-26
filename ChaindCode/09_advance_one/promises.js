4c372729d9eea727f51075e390f183253f5d7077
shivajais25 <shivajais25@gmail.com>
2026-05-26 12:59:17 +0530

    Using of resolve and reject based on some constraints

--- ChaindCode/09_advance_one/promises.js
+++ ChaindCode/09_advance_one/promises.js
@@ -25,4 +25,28 @@ promiseTwo.then(function(user){
 })
 
 
-// using of reject 
+// using of reject and resolve both - then and catch 
+
+const promiseThree = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        let error = false    // if error exist - reject executed or if error false then resolve executed
+
+        if(!error){
+            resolve({username : "SHIVA JAISWAL", password : "SHIVA@JAIS25j"})
+        }
+
+        else{
+            reject({ Error : "Something went wrong!"})
+        }
+
+    }, 5000)
+})
+
+promiseThree.then((user) => {        // then executed when error is false otherwise catch executed
+    console.log(user)
+    return user.username
+}).then((username) => {     // here we get username through chaining method 
+    console.log(username)
+}).catch((error) => {       // catch to handle error
+    console.log(error)
+})    
