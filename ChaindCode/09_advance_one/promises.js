528e0ef776c608ec5631a56af59ced746f2995fa
shivajais25 <shivajais25@gmail.com>
2026-05-26 19:50:23 +0530

    fetching data through link and implemented using async and await

--- ChaindCode/09_advance_one/promises.js
+++ ChaindCode/09_advance_one/promises.js
@@ -87,3 +87,20 @@ async function consumePromiseFour(){
 }
 
 consumePromiseFour()
+
+
+
+// fetching data through fetch in async and await 
+
+async function getAllUsers(){
+    try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/users')
+        const data = await response.json()
+
+        console.log(data)
+    } catch (error) {
+        console.log('E: ', error)
+    }
+}
+
+getAllUsers()
